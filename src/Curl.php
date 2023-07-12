bc782e0a4db97b46301d528c1532f15d6d141aaf
Clairmont RAJAONARISON <clairmont.rajaonarison@gmail.com>
2023-07-12 15:15:14 +0300

    fix : add header to be json

--- src/Curl.php
+++ src/Curl.php
@@ -15,7 +15,7 @@ class Curl
         $this->curl = curl_init();
         curl_setopt($this->curl, CURLOPT_URL, $url);
         curl_setopt($this->curl, CURLOPT_HEADER, TRUE);
-        curl_setopt($this->curl, CURLOPT_HTTPHEADER, array("Authorization: Bearer " . $this->apiKey));
+        curl_setopt($this->curl, CURLOPT_HTTPHEADER, array("Authorization: Bearer " . $this->apiKey, "Content-Type: application/json"));
         curl_setopt($this->curl, CURLOPT_RETURNTRANSFER, TRUE);
     }
     public function curl()
@@ -25,6 +25,8 @@ class Curl
     public function execCurl(array $data = [])
     {
         if (!empty($data)) {
+            curl_setopt($this->curl, CURLOPT_POST, true);
+            curl_setopt($this->curl, CURLOPT_POSTFIELDS, json_encode($data));
         }
         return curl_exec($this->curl);
     }
